afd59fe524a6d0b32f8cc1fff39fd90ee93acd60
dompossebon <dompossebon@gmail.com>
2021-03-25 08:44:08 -0300

    Authenticate Jwt Token-OK

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::post('auth/login', 'Api\\AuthController@login');
+
+Route::group(['middleware' => ['apiJwt']], function (){
+    Route::post('auth/logout', 'Api\\AuthController@logout');
+
+    Route::get('users', 'Api\\UserController@index');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
